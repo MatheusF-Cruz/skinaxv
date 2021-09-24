d19989d7a911b8247f18554ac30798d915150807
israelgms <israelgomes13@hotmail.com>
2021-09-24 18:41:39 -0300

    Criação do componente Navbar

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -1,10 +1,18 @@
 <template>
-  <div id="nav">
+  <Navbar />
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
-  </div>
   <router-view/>
 </template>
+
+<script>
+import Navbar from './components/Navbar.vue'
+export default {
+  components:{
+    Navbar,
+  }
+}
+</script>
 
 <style>
     
