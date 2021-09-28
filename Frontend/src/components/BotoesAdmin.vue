6f282237e6a9443925249a6159ab21ae3a61549d
israelgms <israelgomes13@hotmail.com>
2021-09-28 12:44:32 -0300

    ajustes no backend. Criação de novos components, e implementação de emit's

---
+++ Frontend/src/components/BotoesAdmin.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+      <button class="buttonHS" @click="$emit('emitButtonForm')">{{ buttonForm }}</button>
+      <button class="buttonHS" @click="$emit('emitButtonCar')">{{ buttonCar }}</button>
+    </div>
+</template>
+
+<script>
+
+export default {
+  name: "BotoesAdmin",
+  emits: ['emitButtonForm', 'emitButtonCar'],
+  data() {
+    return {
+      buttonForm: "Cadastrar Veiculos",
+      buttonCar: "Visualizar Veiculos",
+    };
+  },
+};
+</script>
+
+<style scoped>
+.buttonHS{
+  padding: 10px;
+  font-size: 25px;
+}
+</style>
---
+++ Frontend/src/views/Login.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="login">
+        <LoginForm />
+    </div>    
+</template>
+
+<script>
+import LoginForm from '../components/LoginForm.vue'
+
+export default {
+    name: "Login",
+    components: {
+        LoginForm,
+    }
+}
+</script>
